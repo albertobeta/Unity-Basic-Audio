dd18752e493a242556b02c98a60c5db2ec6b8f5f
Alberto Betella <alberto@betella.net>
2015-12-06 12:00:55 +0100

    First commit
    Unity project with 2 scenes.

---
+++ Assets/Scripts/Improviser.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Improviser : MonoBehaviour {
+	
+	AudioSource _audio; 
+
+	// Example: pentatonic blues scale: some semitones changes from a C
+	// 2, 4, 10, 11, 18, 19, 21, 26, 28
+	public int[] semitones;
+
+	void Start () {
+
+		_audio = GetComponent<AudioSource>();
+
+	}
+	
+	void Update () {
+	
+		if (Input.GetKeyDown("space")) {
+
+			PlayNote();
+
+		}
+	}
+
+
+	void PlayNote() {
+
+		float randomSemitone = semitones[Random.Range(0, semitones.Length)];
+		_audio.pitch = Mathf.Pow (2f, randomSemitone/12.0f);
+		_audio.Play ();
+		Debug.Log("Bliiing (" +randomSemitone+ ")");
+
+	}
+
+}
---
+++ Assets/Scripts/bouncingBall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bouncingBall : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+
+		Debug.Log("Collision");
+
+		GetComponent<AudioSource>().Play();
+
+	}
+
+}
